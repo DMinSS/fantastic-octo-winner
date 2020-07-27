8cb7fb9c0811c737a5aa93a5d54e79b4f5174e49
DminSS <noreply.its.cloud@gmail.com>
2020-07-27 20:10:38 +0300

    add server

---
+++ app.js
@@ -0,0 +1,26 @@
+const {add, minus} = require('./module');
+const express = require('express');
+const bodyparser = require('body-parser');
+const app = express();
+
+app.use(bodyparser.urlencoded({extended: false}));
+
+app.use(bodyparser.json());
+
+app.use(express.static('public'));
+
+app.get('/user/:user_name', (req, res, next) => {
+    res.status(200).end(`Hello, ${req.params.user_name}`);
+});
+
+app.post('/comments', (req, res, next) => {
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end(JSON.stringify(req.body, null, 2));
+});
+
+app.post('/form', (req, res, next) => {
+    console.log(JSON.stringify(req.body, null, 4));
+    res.redirect('/');
+})
+
+app.listen(3000);
